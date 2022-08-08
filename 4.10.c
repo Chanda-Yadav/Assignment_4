0b51c7afd509133e38c5eb6fb7488a55bd59dbcf
Chanda Yadav <88459605+Chanda-Yadav@users.noreply.github.com>
2022-08-08 23:57:43 +0530

    Add files via upload

---
+++ 4.10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int i=1;
+   for(i=1;i<=10;i++)
+        printf("5*%d=%d\n",i,5*i);
+
+}
+
+
---
+++ 4.7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int i=10;
+   for(i=10;i>=1;i--)
+        printf("%d ",(2*i));
+
+}
+
